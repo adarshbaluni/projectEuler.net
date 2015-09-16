346c356f460c9ac00fdf11478be6350c2277b77f
Adarsh Baluni <adarshbaluni@gmail.com>
2015-09-16 12:34:14 -0700

    Project Euler created (projecteuler.net). Problem1 added

---
+++ problem1/problem1.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def findSumOfMultiples():
+
+	sumOfMultiples = 0
+
+	for value in range(1,1000):
+		if( value % 3 == 0 or value % 5 == 0):
+			sumOfMultiples += value			
+			
+	return sumOfMultiples
+
+
+
+if __name__ == "__main__":
+
+	result = findSumOfMultiples()
+
+	print "Sum of multiples of 3 or 5 below 1000 is %r" % result
